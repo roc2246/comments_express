ac9155817847beaf51ac2f2c18c4c15621b7eeb3
Riley <riley.childs@yahoo.com>
2024-01-17 07:30:23 -0500

    Installed dotenv

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+
 // Set up middleware for static files (CSS, JS, images, etc.)
 app.use(express.static("views"));
 
