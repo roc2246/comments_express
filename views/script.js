183efe9a5934bd5d8400589ec9ed8bfc2c51772f
Riley <riley.childs@yahoo.com>
2024-01-23 17:11:13 -0500

    Included vote buttons

--- views/script.js
+++ views/script.js
@@ -1,3 +1,9 @@
+function renderVote(mode) {
+  return `<button class="vote vote--${mode}">
+  <img class="vote__button" src="./images/icon-${mode}.svg"/>
+  </button>`;
+}
+
 function renderComment(comment) {
   return `
   <div class="comment">
@@ -5,29 +11,34 @@ function renderComment(comment) {
     <span class="comment__username">${comment.user.username}</span>
     <span class="comment__createdAt">${comment.createdAt}</span>
     <p class="comment__content">${comment.content}</p>
-    <span class="comment__score">Score: ${comment.score}</span>
+    <span class="comment__score">${renderVote("plus")}${
+    comment.score
+  }${renderVote("minus")}</span>
   </div>
     `;
 }
 
 function renderReplies(replies) {
-  return replies
-    .map(
-      (reply) => `
+  if (replies.length !== 0) {
+    return `
       <div class="comments comments--replies">
+      <hr class="comments__ruler"/>${replies
+        .map(
+          (reply) => `
       <div class="comment comment--reply">
         <img class="comment__avatar" src="${reply.user.image.png}">
         <span class="comment__username">${reply.user.username}</span>
         <span class="comment__createdAt">${reply.createdAt}</span>
         <p class="comment__content">${reply.content}</p>
         <span class="comment__score">Score: ${reply.score}</span>
-      </div>
-      </div>
-    `
-    )
-    .join("");
+      </div>`
+        )
+        .join("")}
+    </div>`;
+  } else {
+    return "";
+  }
 }
-
 
 function renderComments(comments) {
   const container = document.getElementsByClassName("comments")[0];
@@ -45,7 +56,6 @@ async function fetchComments() {
   try {
     const response = await fetch("comments");
     const comments = await response.json();
-    console.log(comments);
     renderComments(comments);
   } catch (error) {
     console.error("Error while fetching comments:", error);
